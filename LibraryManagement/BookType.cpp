cf68b9cdf92815437a1e77263b17787290d284f0
qushaowei <qushaowei111@outlook.com>
2019-10-14 16:29:25 +0800

    update

--- LibraryManagement/BookType.cpp
+++ LibraryManagement/BookType.cpp
@@ -10,3 +10,23 @@ BookType::BookType()
 BookType::~BookType()
 {
 }
+
+int BookType::getId()
+{
+	return this->id;
+}
+
+void BookType::setId(const int id)
+{
+	this->id = id;
+}
+
+string BookType::getName()
+{
+	return string(this->name);
+}
+
+void BookType::setName(const string &name)
+{
+	strcpy(this->name, name.c_str());
+}
--- LibraryManagement/Record.cpp
+++ LibraryManagement/Record.cpp
@@ -20,3 +20,63 @@ void Record::setId(const int id)
 {
 	this->id = id;
 }
+
+int Record::getUserId()
+{
+
+}
+
+void Record::setUserId(const int userId)
+{
+
+}
+
+int Record::getBookId()
+{
+
+}
+
+void Record::setBookId(const int bookId)
+{
+
+}
+
+string Record::getDate()
+{
+
+}
+
+void Record::setDate(const string &date)
+{
+
+}
+
+int Record::getDuration()
+{
+
+}
+
+void Record::setDuration(const int duration)
+{
+
+}
+
+string Record::getReturnDate()
+{
+
+}
+
+void Record::setReturnDate(const string &returnDate)
+{
+
+}
+
+Record::RecordStatus Record::getStatus()
+{
+
+}
+
+void Record::setStatus(const RecordStatus status)
+{
+
+}
