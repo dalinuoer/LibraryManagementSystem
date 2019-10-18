6b7e6e5932c2081de42d0ea547a0cce94bcd32fd
Qin-Hao <qh530658481@outlook.com>
2019-10-18 20:44:38 +0800

    Fix some bugs about console command.

--- LibraryManagement/main.cpp
+++ LibraryManagement/main.cpp
@@ -4,31 +4,13 @@
 #include "Book.h"
 #include "Record.h"
 #include "UserDao.h"
+#include "consoleCommand.h"
 
 using namespace std;
 
 int main()
 {
-	User user;
-	user.setId(1);
-	user.setName("Hi");
-	user.setType(User::STUDENT);
-	user.setStatus(User::NORMAL);
-	
-	
-	UserDao userDao("data/User.dat");
-	userDao.insertUser(user);
-
-	user.setId(2);
-	user.setName("Hello");
-	userDao.insertUser(user);
-
-	user.setId(3);
-	user.setName("World");
-	userDao.insertUser(user);
-
-	user.setName("Hey");
-	userDao.updateUser(3, user);
+	console();
 
 	return 0;
 }
