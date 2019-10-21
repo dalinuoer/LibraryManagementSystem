9e801e57743d9f129475cc379c0167295893e8d2
mlyue <43882108+mlyue@users.noreply.github.com>
2019-10-21 21:12:00 +0800

    Add files via upload

--- LibraryManagement/UserService.cpp
+++ LibraryManagement/UserService.cpp
@@ -1,34 +1,40 @@
 #include "UserService.h"
 #include <string>
 #include <vector>
+#include <iostream>
 #include "User.h"
+#include"UserDao.h"
+//#include"RecordService.h"
 using namespace std;
 
-UserDao userDao = new UserDao();
+UserDao* userDao = new UserDao("filename.txt");
 
 bool UsersService::addUser(User user)
 {
-	User u = userDao.findByName(user.getName());
+	User u = userDao->findUserById(user.getId(), found);
 	if (u != NULL) { // 如果要添加的name已经存在
 		return -1;
 	}
-	return userDao.addUser(user);
+	return userDao->insertUser(user);
 
 }
 
 bool UsersService::delUser(int id)
 {
-	return userDao.delUser(id);
+	return userDao->deleteUser(id);
 
 }
 
 vector<User> UsersService::findAllUser()
 {
-	return userDao.findAllUser();
+	return userDao->findAllUser();
 }
 
 bool UsersService::changeUserinfo(int userid, User user)
 {
-	return userDao.updateUser(userid, user);
+	return userDao->updateUser(userid, user);
 }
+
+
+
 
