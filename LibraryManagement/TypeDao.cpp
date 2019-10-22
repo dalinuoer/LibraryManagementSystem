4be2c7dece2f7ee7594fb517f46d458aaf3ced50
Nisces <Nisces0304@outlook.com>
2019-10-22 21:17:15 +0800

    type

--- LibraryManagement/TypeDao.cpp
+++ LibraryManagement/TypeDao.cpp
@@ -1,12 +1,31 @@
 #include "TypeDao.h"
 
-
-
-TypeDao::TypeDao()
+TypeDao::TypeDao() : dao("data/Type.dat")
 {
 }
 
 
 TypeDao::~TypeDao()
 {
+}
+
+bool TypeDao::insertType(const BookType &type)
+{
+	return dao.insert(type);
+}
+
+
+bool TypeDao::updateType(int id, const BookType &type)
+{
+	return dao.update(id, type);
+}
+
+bool TypeDao::deleteType(int id)
+{
+	return dao.del(id);
+}
+
+vector<BookType> TypeDao::findAllType()
+{
+	return dao.findAll();
 }
--- LibraryManagement/TypeDao.h
+++ LibraryManagement/TypeDao.h
@@ -1,8 +1,20 @@
 #pragma once
+#include "Dao.hpp"
+#include "BookType.h"
+#include <vector>
+
+using namespace std;
+
 class TypeDao
 {
 public:
 	TypeDao();
 	~TypeDao();
+	bool insertType(const BookType &type);
+	bool updateType(int id, const BookType &type);
+	bool deleteType(int id);
+	vector<BookType> findAllType();
+private:
+	Dao<BookType> dao;
 };
 
